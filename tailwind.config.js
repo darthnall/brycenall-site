53fe887183c0964aa76f732c1a9ab93551873287
Blake Nall <blakenall@proton.me>
2024-04-18 16:59:38 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+        "./home/templates/home/*.html",
+        "./home/templates/home/**/*.html",
+        "./shop/templates/home/*.html",
+        "./shop/templates/home/**/*.html",
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
